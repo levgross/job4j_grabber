c60b1001227dad92cc2dccda5c917cdc270ef6ad
Grossevich Lev <levgross@gmail.com>
2022-05-22 15:01:50 +0400

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,7 +3,6 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
@@ -14,13 +13,16 @@ import static org.quartz.TriggerBuilder.*;
 
 public class AlertRabbit {
     public static void main(String[] args) {
-        try {
-            init();
+        try (Connection cn = init(config())) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", cn);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Rabbit.interval())
+                    .withIntervalInSeconds(Rabbit.interval(config()))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -34,27 +36,32 @@ public class AlertRabbit {
         }
     }
 
-    private static Connection connect;
-
-    public static void init() {
+    public static Properties config() {
+        Properties config = new Properties();
         try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-            Properties config = new Properties();
             config.load(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return config;
+    }
+
+    public static Connection init(Properties config) throws Exception {
+        Connection connect;
             Class.forName(config.getProperty("driver-class-name"));
             connect = DriverManager.getConnection(
                     config.getProperty("url"),
                     config.getProperty("username"),
                     config.getProperty("password")
             );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        return connect;
     }
 
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            try (PreparedStatement ps = connect.prepareStatement("insert into rabbit(created_date) values (?)")) {
+            Connection cn = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement ps = cn.prepareStatement("insert into rabbit(created_date) values (?)")) {
                 ps.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
                 ps.execute();
             } catch (Exception e) {
@@ -63,16 +70,8 @@ public class AlertRabbit {
             System.out.println("Rabbit runs here ...");
         }
 
-        public static int interval() {
-            int rsl = 0;
-            try (InputStream in = Rabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-                Properties config = new Properties();
-                config.load(in);
-                rsl = Integer.parseInt(config.getProperty("rabbit.interval"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return rsl;
+        public static int interval(Properties config) {
+            return Integer.parseInt(config.getProperty("rabbit.interval"));
         }
     }
 }

e417123b811260357ce512fe88a755d3d8e2c3b1
Grossevich Lev <levgross@gmail.com>
2022-05-23 11:09:39 +0400

    2.1. Преобразование даты

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
+
     @Override
     public LocalDateTime parse(String parse) {
         return ZonedDateTime.parse(parse).toLocalDateTime();
